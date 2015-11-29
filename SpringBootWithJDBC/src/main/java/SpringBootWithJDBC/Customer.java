1609b78860dd26dcb65c4836ec4e01dc8de8699f
tykowale <tykowale@gmail.com>
2015-11-29 13:43:21 -0600

    example spring boot connect to databse with jdbc

---
+++ SpringBootWithJDBC/src/main/java/SpringBootWithJDBC/Customer.java
@@ -0,0 +1,37 @@
+package SpringBootWithJDBC;
+
+/**
+ * Created by tylerkowalewski on 11/29/15.
+ */
+public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName
+        );
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
